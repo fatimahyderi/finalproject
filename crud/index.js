1792a73562c984ef8be68495bd86b633211b3027
fatimahyderi <fatimakhurram84@gmail.com>
2022-07-19 14:41:57 +0500

    env

--- crud/index.js
+++ crud/index.js
@@ -19,7 +19,7 @@ var corsOptions = {
   optionsSuccessStatus: 200, // some legacy browsers (IE11, variousSmartTVs) choke on 204
   credentials:true
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}));
 
